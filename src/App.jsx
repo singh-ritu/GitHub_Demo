2450d6c12878742973e98f53c6c21fcfe7acf4ba
Ritu <singhritu241101@gmail.com>
2024-12-13 17:18:08 +0530

    fix: input is altered

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,6 @@ function App() {
   };
 
   const fetchData = async (e) => {
-    // fetch("https://api.github.com/users/singh-ritu")
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data));
     e.preventDefault();
     const res = await axios.get(`https://api.github.com/users/${username}`);
     console.log(res);
@@ -120,37 +117,3 @@ function App() {
 }
 
 export default App;
-// {
-//   "login": "singh-ritu",
-//   "id": 82505170,
-//   "node_id": "MDQ6VXNlcjgyNTA1MTcw",
-//   "avatar_url": "https://avatars.githubusercontent.com/u/82505170?v=4",
-//   "gravatar_id": "",
-//   "url": "https://api.github.com/users/singh-ritu",
-//   "html_url": "https://github.com/singh-ritu",
-//   "followers_url": "https://api.github.com/users/singh-ritu/followers",
-//   "following_url": "https://api.github.com/users/singh-ritu/following{/other_user}",
-//   "gists_url": "https://api.github.com/users/singh-ritu/gists{/gist_id}",
-//   "starred_url": "https://api.github.com/users/singh-ritu/starred{/owner}{/repo}",
-//   "subscriptions_url": "https://api.github.com/users/singh-ritu/subscriptions",
-//   "organizations_url": "https://api.github.com/users/singh-ritu/orgs",
-//   "repos_url": "https://api.github.com/users/singh-ritu/repos",
-//   "events_url": "https://api.github.com/users/singh-ritu/events{/privacy}",
-//   "received_events_url": "https://api.github.com/users/singh-ritu/received_events",
-//   "type": "User",
-//   "site_admin": false,
-//   "name": "Ritu ",
-//   "company": null,
-//   "blog": "",
-//   "location": null,
-//   "email": null,
-//   "hireable": null,
-//   "bio": "Hey there 👋\r\nI'm  Ritu , a coding enthusiast and technology explorer. 🌱\r\nAs a beginner , I'm on an enchanting journey to learn and grow. ",
-//   "twitter_username": null,
-//   "public_repos": 7,
-//   "public_gists": 0,
-//   "followers": 0,
-//   "following": 0,
-//   "created_at": "2021-04-14T09:05:59Z",
-//   "updated_at": "2023-08-17T07:31:32Z"
-// }
